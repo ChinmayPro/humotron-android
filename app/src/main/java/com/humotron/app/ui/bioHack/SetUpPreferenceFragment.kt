202eb255cde598776efdc8494951eebf021e4ccc
Dishant S <dnsavaliya96@gmail.com>
2026-09-22 17:18:02 +0530

    refactor: rename preference view IDs and expand click listeners in SetUpPreferenceFragment

--- app/src/main/java/com/humotron/app/ui/bioHack/SetUpPreferenceFragment.kt
+++ app/src/main/java/com/humotron/app/ui/bioHack/SetUpPreferenceFragment.kt
@@ -32,42 +32,48 @@ class SetUpPreferenceFragment : BaseFragment(R.layout.fragment_set_up_preference
         binding.header.ivNuggets.setOnClickListener(this)
         binding.header.ivBooks.setOnClickListener(this)
         binding.header.ivProgress.setOnClickListener(this)
+        binding.ivPrefNuggets.setOnClickListener(this)
+        binding.tvPrefNuggets.setOnClickListener(this)
+        binding.ivPrefBooks.setOnClickListener(this)
+        binding.tvPrefBooks.setOnClickListener(this)
+        binding.ivPrefTrack.setOnClickListener(this)
+        binding.tvPrefTracks.setOnClickListener(this)
         binding.btnSubmit.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
         when (p0) {
-            binding.header.ivNuggets -> {
-                binding.tvNuggets.isChecked = binding.tvNuggets.isChecked.not()
-                binding.header.ivNuggets.setImageResource(
-                    if (binding.tvNuggets.isChecked) {
-                        R.drawable.ic_nuggets_checked
-                    } else {
-                        R.drawable.ic_nuggets_disable
-                    }
-                )
+            binding.header.ivNuggets, binding.ivPrefNuggets, binding.tvPrefNuggets -> {
+                binding.tvPrefNuggets.isChecked = binding.tvPrefNuggets.isChecked.not()
+                val iconRes = if (binding.tvPrefNuggets.isChecked) {
+                    R.drawable.ic_nuggets_checked
+                } else {
+                    R.drawable.ic_nuggets_disable
+                }
+                binding.ivPrefNuggets.setImageResource(iconRes)
+                binding.header.ivNuggets.setImageResource(iconRes)
             }
 
-            binding.header.ivBooks -> {
-                binding.tvBooks.isChecked = binding.tvBooks.isChecked.not()
-                binding.header.ivBooks.setImageResource(
-                    if (binding.tvBooks.isChecked) {
-                        R.drawable.ic_books_checked
-                    } else {
-                        R.drawable.ic_books_disable
-                    }
-                )
+            binding.header.ivBooks, binding.ivPrefBooks, binding.tvPrefBooks -> {
+                binding.tvPrefBooks.isChecked = binding.tvPrefBooks.isChecked.not()
+                val iconRes = if (binding.tvPrefBooks.isChecked) {
+                    R.drawable.ic_books_checked
+                } else {
+                    R.drawable.ic_books_disable
+                }
+                binding.ivPrefBooks.setImageResource(iconRes)
+                binding.header.ivBooks.setImageResource(iconRes)
             }
 
-            binding.header.ivProgress -> {
-                binding.tvTracks.isChecked = binding.tvTracks.isChecked.not()
-                binding.header.ivProgress.setImageResource(
-                    if (binding.tvTracks.isChecked) {
-                        R.drawable.ic_biohack_progress_checked
-                    } else {
-                        R.drawable.ic_biohack_progress_disable
-                    }
-                )
+            binding.header.ivProgress, binding.ivPrefTrack, binding.tvPrefTracks -> {
+                binding.tvPrefTracks.isChecked = binding.tvPrefTracks.isChecked.not()
+                val iconRes = if (binding.tvPrefTracks.isChecked) {
+                    R.drawable.ic_biohack_progress_checked
+                } else {
+                    R.drawable.ic_biohack_progress_disable
+                }
+                binding.ivPrefTrack.setImageResource(iconRes)
+                binding.header.ivProgress.setImageResource(iconRes)
             }
 
             binding.btnSubmit -> {
